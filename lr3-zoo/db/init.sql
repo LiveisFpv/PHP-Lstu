95fd4d7256b8ea94dd9f9d8644fb2f380133a2bb
LiveisFpv <nasonovnicolai@gmail.com>
2025-02-18 09:45:36 +0300

    add: init db

--- lr3-zoo/db/init.sql
+++ lr3-zoo/db/init.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS animal
+DROP TABLE IF EXISTS 'animal';
 
-CREATE TABLE animal(
+CREATE TABLE 'animal'(
     animal_id serial NOT NULL,
     animal_name varchar(255),
     animal_gender varchar(1),
