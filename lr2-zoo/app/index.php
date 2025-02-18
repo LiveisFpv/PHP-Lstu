3b49b333e652049d16136ccddc26f1b1907a3100
LiveisFpv <nasonovnicolai@gmail.com>
2025-02-18 10:00:24 +0300

    add: style

--- lr2-zoo/app/index.php
+++ lr2-zoo/app/index.php
@@ -4,21 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>lr2</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Хакеры хотят данные</h1>
-
     <form action="form.php" method="post">
+        <h1>Главный зоопарк у дома</h1>
         <label for="animal-name">Название животного:</label>
-        <input type="text" id="animal-name" name="animal-name" required><br><br>
+        <input type="text" id="animal-name" name="animal-name" required>
         <label for="animal-gender">Пол животного:</label>
-        <input type="text" id="animal-gender" name="animal-gender" required><br><br>
+        <input type="text" id="animal-gender" name="animal-gender" required>
         <label for="animal-age">Возвраст животного:</label>
-        <input type="number" id="animal-age" name="animal-age" required><br><br>
+        <input type="number" id="animal-age" name="animal-age" required>
         <label for="cage">Клетка:</label>
-        <input type="text" id="cage" name="cage" required><br><br>
+        <input type="text" id="cage" name="cage" required>
         <label for="care">Уход:</label>
-        <input type="text" id="care" name="care" required><br><br>
+        <input type="text" id="care" name="care" required>
         <input type="submit" value="Отправить">
     </form>
 </body>
