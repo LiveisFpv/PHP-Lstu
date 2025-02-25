90622b9168c1908e70e1ef6e6f4ac10a1538d01a
LiveisFpv <nasonovnicolai@gmail.com>
2025-02-25 14:14:55 +0300

    add: validation form on back, alert on front

--- lr2-zoo/app/form.php
+++ lr2-zoo/app/form.php
@@ -1,4 +1,5 @@
 <?php
+    session_start();
     if ($_SERVER['REQUEST_METHOD'] === 'POST'){
         $animal_name = trim($_POST['animal-name']?? '');
         $animal_gender = trim($_POST['animal-gender']?? '');
@@ -6,26 +7,56 @@
         $cage = trim($_POST['cage']?? '');
         $care = trim($_POST['care']?? '');
 
-        $csvFile = 'data.csv';
+        $error=false;
+        $message ="";
+        if(empty($animal_name)){
+            $error = true;
+            $message = $message . "Название животного не может быть пустым.\n";
+        }
+        elseif (!preg_match('/^[a-zA-Zа-яА-ЯёЁ\s\-]+$/u', $animal_name)) {
+            $error = true;
+            $message = $message . "Название животного может содержать только буквы, пробелы и дефисы.\n";
+        }
+        
+        if (empty($animal_gender)) {
+            $error = true;
+            $message = $message . "Пол животного не может быть пустым.\n";
+        } elseif ($animal_gender != "М" || $animal_age != "Ж") {
+            $error = true;
+            $message = $message . "Пол животного должен быть 'м' (мужской) или 'ж' (женский).\n";
+        }
 
-        $dataRow = [$animal_name,$animal_gender ,$animal_age,$cage,$care];
-        if (($file=fopen($csvFile, 'a'))!==false){
-            fputcsv($file,$dataRow);
-            fclose($file);
-            $message = 'Данные успешно сохранены';
-        } else {
-            $message = 'Ошибка при сохранении';
+        if (!filter_var($cage, FILTER_VALIDATE_INT, ["options" => ["min_range" => 1, "max_range" => 1000]])) {
+            $error = true;
+            $message = $message . "Номер клетки должен быть целым числом от 1 до 1000.\n";
+        }
+
+        if (empty($care)) {
+            $error = true;
+            $message = $message . "Поле ухода не может быть пустым.\n";
+        } elseif (!preg_match('/^[a-zA-Zа-яА-ЯёЁ\s\-]+$/u', $care)) {
+            $error = true;
+            $message = $message . "Поле ухода может содержать только буквы, пробелы и дефисы.\n";
+        }
+        
+
+        
+
+        if ($error===false){
+            $csvFile = 'data.csv';
+
+            $dataRow = [$animal_name,$animal_gender ,$animal_age,$cage,$care];
+            if (($file=fopen($csvFile, 'a'))!==false){
+                fputcsv($file,$dataRow);
+                fclose($file);
+                $message = 'Данные успешно сохранены';
+            } else {
+                $message = 'Ошибка при сохранении';
+            }
         }
     }
+    $_SESSION["message"]= $message;
+    //Redirect
+    header("Location: /index.php");
+    exit;
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    Данные приняты
-</body>
-</html>
--- lr2-zoo/app/index.php
+++ lr2-zoo/app/index.php
@@ -1,3 +1,6 @@
+<?php
+    session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +10,15 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <script>
+        window.onload = function() {
+            let message = <?php echo json_encode($_SESSION["message"] ?? ""); ?>;
+            if (message != ""){
+                alert(message);
+            }
+            <?php $_SESSION["message"]="";?>
+        };
+    </script>
     <form action="form.php" method="post">
         <h1>Главный зоопарк у дома</h1>
         <label for="animal-name">Название животного:</label>
@@ -14,9 +26,9 @@
         <label for="animal-gender">Пол животного:</label>
         <input type="text" id="animal-gender" name="animal-gender" required>
         <label for="animal-age">Возвраст животного:</label>
-        <input type="number" id="animal-age" name="animal-age" required>
+        <input type="number" id="animal-age" name="animal-age" required min="1">
         <label for="cage">Клетка:</label>
-        <input type="text" id="cage" name="cage" required>
+        <input type="number" id="cage" name="cage" required min="1">
         <label for="care">Уход:</label>
         <input type="text" id="care" name="care" required>
         <input type="submit" value="Отправить">
